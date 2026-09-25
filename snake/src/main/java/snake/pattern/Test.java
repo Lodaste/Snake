00a39629f39131628626fd4117899e00cbafc1bb
Louis RICHARD <louis.richard.045@gmail.com>
2026-09-25 21:17:36 +0200

    Ajout de Logs et de commentaires

--- snake/src/main/java/snake/pattern/Test.java
+++ snake/src/main/java/snake/pattern/Test.java
@@ -2,15 +2,29 @@ package snake.pattern;
 
 import snake.pattern.archi.SimpleGame;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Classe pour tester l'application
+ */
 public class Test 
 {
+    /**
+     * Logger de classe
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
     public static void main(String[] args) 
     {
-        System.out.println("Hello world!");
         SimpleGame game = new SimpleGame(10, Long.valueOf(1000));
+
+        logger.info(("Démmarage du test"));
 
         game.init();
         game.step();
         game.run();
+
+        logger.info("Fin du test");
     }
 }
--- snake/src/main/java/snake/pattern/archi/Game.java
+++ snake/src/main/java/snake/pattern/archi/Game.java
@@ -1,11 +1,39 @@
 package snake.pattern.archi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Classe abstraite représentant une Stratégie générale
+ * Game
+ */
 public abstract class Game implements Runnable
 {
+    private static final Logger logger = LoggerFactory.getLogger(Game.class);
+    /**
+     * Le tour courrent
+     */
     int curentTurn;
+
+    /**
+     * Le nombre maximum de tours
+     */
     int maxTurn;
+
+    /**
+     * Drapeau boolean indiquant si le jeu tourne encore
+     */
     boolean isRunning;
+
+    /**
+     * Temps en milisecondes entre chaque tours
+     */
     Long time;
+
+    /**
+     * Une instance de jeu
+     */
     Thread thread;
 
     public Game (int maxTurn, long time)
@@ -14,6 +42,9 @@ public abstract class Game implements Runnable
         this.time = time;
     }
 
+    /**
+     * Méthode poour initialiser mettre à zéro certains paramêtre d'une partie
+     */
     public void init()
     {
         curentTurn = 0;
@@ -22,8 +53,14 @@ public abstract class Game implements Runnable
         initializeGame();
     }
 
+    /**
+     * Initialise une partie
+     */
     protected abstract void initializeGame();
 
+    /**
+     * Initialise une instance de jeu (thread)
+     */
     public void launch()
     {
         isRunning = true;
@@ -31,11 +68,12 @@ public abstract class Game implements Runnable
         thread.start();
     }
     
+    /**
+     * Méthode se pérocupant du prochain de lancer leprochain tour de jeu si possible
+     */
     public void step()
     {
         curentTurn ++;
-
-        
         if (isGameOver() || curentTurn == maxTurn)
         {
             isRunning = false;
@@ -45,20 +83,36 @@ public abstract class Game implements Runnable
         {
             takeTurn();
         }
-        
     }
 
+    /**
+     * Prochain tour de jeu
+     */
     protected abstract void takeTurn();
 
+    /**
+     * Méthode pour terminer une partie
+     */
     protected abstract void gameOver();
 
+    /**
+     * Méthode indiquant si la partie est finie
+     * @return l'état de la partie
+     */
     protected abstract boolean isGameOver();
 
+    /**
+     * Met en pause le jeu
+     */
     void pause()
     {
         isRunning = false;
     }
 
+    /**    (non-Javadoc)
+     * Méthode core d'une partie invoquant le possible tour à venir
+     * @see java.lang.Runnable#run()
+     */
     public void run()
     {
         while(isRunning)
@@ -69,7 +123,7 @@ public abstract class Game implements Runnable
                 Thread.sleep(time);
             } catch (InterruptedException e) 
             {
-                System.out.println("Erreur time"); // Throw une erreur ?
+                logger.error("Le programme ne s'est pas endormis", e);
             }
         }
     }
